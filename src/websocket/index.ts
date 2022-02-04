48c2e6c72e83e22610fed9ef090cf255a7cf4437
jialinjing <jialinjing@360.cn>
2022-02-05 01:39:16 +0800

    fix: Yjs websocket connection

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,12 +1,15 @@
 import { Server } from 'http'
 import { Server as HTTPSServer } from 'https'
 import * as WebSocket from 'ws'
-export class WebSocketServer{
+import { setupWSConnection } from './utils'
+
+export class WebSocketServer {
     wsServer
-    constructor(server: Server| HTTPSServer){
-        this.wsServer = new WebSocket.Server({
-            server,
-            path:'/ws/doc'
-        })
+    constructor(server: Server | HTTPSServer) {
+    this.wsServer = new WebSocket.Server({
+        server,
+        path: '/ws/doc',
+    })
+    this.wsServer.on('connection', setupWSConnection)
     }
 }
